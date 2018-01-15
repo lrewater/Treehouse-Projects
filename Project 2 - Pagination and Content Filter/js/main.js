86518009f9c8f79996d047a288a6da647d85a955
tylerben <ben.tyler@lrewater.com>
2018-01-14 21:07:59 -0700

    Draft search functionality

--- Project 2 - Pagination and Content Filter/js/main.js
+++ Project 2 - Pagination and Content Filter/js/main.js
@@ -1,8 +1,11 @@
 (function() {
 
 	var $page = $( '.page' ),
+		$list = $( 'ul.student-list' ),
 		$listItems = $( '.student-item' ),
 		$pagination = $( '.pagination' ),
+		$search = $( '.student-search input' ),
+		$searchBtn = $( '.student-search button' ),
 		listLength = $listItems.length,
 		pagesNeeded = calcPagesNeeded(listLength);
 
@@ -16,15 +19,13 @@
 
 	function showPage(pageNo, listItems) {
 		var upperBound = pageNo * 10,
-			lowerBound = upperBound - 9;
-
+			lowerBound = upperBound - 10;
 		for (let i = 0; i < listItems.length; i++) {
 			if (i >= lowerBound && i <= upperBound) {
 				$( listItems[i] ).show();		
 			} else {
 				$( listItems[i] ).hide();
 			}
-
 		}
 	}
 
@@ -42,6 +43,69 @@
 		}
 		$( pageLinks[pageNo - 1] ).find('a').addClass('active');
 	}
+
+	function getSearchTerms() {
+		return $search.val().toLowerCase();
+	}
+
+	function displaySearchResults() {
+		var matched = [], // array that will hold list of matched students
+			students = []; // array that will hold student names and email addresses
+
+		// Obtain the value of the search input
+		var searchTerm = getSearchTerms();
+    	// remove the previous page link section 
+    	removePagination();   
+    	// Loop over the student list, and for each student…
+			// ...obtain the student’s name…
+			// ...and the student’s email…
+		for (let i = 0; i < $listItems.length; i++) {
+			$( $listItems[i] ).hide();
+			let student = $( $listItems[i] );
+				students.push({name: student.find('h3').text().toLowerCase(), email: student.find('span.email').text().toLowerCase(), index: i});
+		}
+		// ...if the search value is found inside either email or name…
+    		// ...add this student to list of “matched” student
+    	matched = students.filter(function (d) {
+    		return d.name.includes(searchTerm) ||
+    			   d.email.includes(searchTerm);
+    	});
+
+    	for (let i = 0; i < matched.length; i++) {
+    		var index = matched[i].index;
+    		$( $listItems[index] ).show();
+    	}
+
+    	let searchCount = matched.length;
+
+    	$list.find('.search-alert').remove();
+    	if (matched.length === 0) {    		
+    		$list.append('<h3 class="search-alert">No results found for current search</h3>')
+    	} else {
+    		buildPagination(calcPagesNeeded(searchCount));
+    	}
+		
+    	// If there’s no “matched” students…
+           // ...display a “no student’s found” message
+   		// If over ten students were found…
+           // ...call appendPageLinks with the matched students
+	   // Call showPage to show first ten students of matched list
+
+	}
+
+	function refreshResults() {
+		var searchTerm = getSearchTerms();
+		if ( searchTerm === "" ) {
+			for (let i = 0; i < $listItems.length; i++) {
+				$( $listItems[i] ).show();
+			}
+			buildPagination(pagesNeeded);
+		}
+	}
+
+	$search.on("input", displaySearchResults)
+	$search.on("input", refreshResults)
+	$searchBtn.on("click", displaySearchResults)
 
 	/**
 	 * Function used to remove pagination links if they exist
@@ -66,8 +130,8 @@
 		$page.append(html);
 		showPage(1, $( 'li.student-item' ))
 		$( '.pagination ul').on("click", getPage)
-		setActivePage(1, $( '.pagination li' ));		
-		//setActivePage(0);
+		setActivePage(1, $( '.pagination li' ));	
+		$pagination = $( '.pagination' ); //u update DOM reference
 	}
 
 	buildPagination(pagesNeeded);
