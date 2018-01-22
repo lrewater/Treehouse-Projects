950d9c42e21cb4afc4bc1bc175e27d8be2f444a0
tylerben <ben.tyler@lrewater.com>
2018-01-22 07:06:30 -0700

    Add suport for student search.

--- Project 2 - Pagination and Content Filter/js/main.js
+++ Project 2 - Pagination and Content Filter/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-
+	// declare variables
 	var $page = $( '.page' ),
 		$list = $( 'ul.student-list' ),
 		$listItems = $( '.student-item' ),
@@ -17,11 +17,17 @@
 		return Math.ceil(len/10);
 	}
 
+	/**
+	 * Function used to show the currently selected page in the directory
+	 * @param  {number} pageNo    [The page to show]
+	 * @param  {array} listItems [array of objects representing the student list items ]
+	 */
 	function showPage(pageNo, listItems) {
-		var upperBound = pageNo * 10,
-			lowerBound = upperBound - 10;
+		var upperBound = pageNo * 10, // upperbound of list item index
+			lowerBound = upperBound - 10; // lowerbound of list item index
+
 		for (let i = 0; i < listItems.length; i++) {
-			if (i >= lowerBound && i <= upperBound) {
+			if (i >= lowerBound && i < upperBound) {
 				$( listItems[i] ).show();		
 			} else {
 				$( listItems[i] ).hide();
@@ -29,6 +35,10 @@
 		}
 	}
 
+	/**
+	 * Function used to retrieve the user's selected page
+	 * @param  {objec} e [Event object]
+	 */
 	function getPage(e) {
 		e.preventDefault();
 		var activePage = e.target;
@@ -37,6 +47,11 @@
 		showPage(pageNo, $( 'li.student-item' ));
 	}
 
+	/**
+	 * Function used to style the pagination link for the user's selected page
+	 * @param {number} pageNo    [the user's selected page number]
+	 * @param {object} pageLinks [CSS selector for the pagination list]
+	 */
 	function setActivePage(pageNo, pageLinks) {
 		for (var i = 0; i < pageLinks.length; i++) {
 			$( pageLinks[i] ).find('a').removeClass("active");
@@ -44,21 +59,31 @@
 		$( pageLinks[pageNo - 1] ).find('a').addClass('active');
 	}
 
+	/**
+	 * Function used to retrieve user's search value from input
+	 * Response is converted to lowercase for easier comparison
+	 * @return {string} [search input value]
+	 */
 	function getSearchTerms() {
 		return $search.val().toLowerCase();
 	}
 
+	/**
+	 * Function used to render list items that match the user's search criteria
+	 */
 	function displaySearchResults() {
-		var matched = [], // array that will hold list of matched students
-			students = []; // array that will hold student names and email addresses
+		var students = [], // array that will hold student names and email addresses
+			matched  = [], // array that will hold list of matched students
+			searchTerm = "";
 
 		// Obtain the value of the search input
-		var searchTerm = getSearchTerms();
+		searchTerm = getSearchTerms();
     	// remove the previous page link section 
-    	removePagination();   
+    	removePagination();    	   
     	// Loop over the student list, and for each student…
 			// ...obtain the student’s name…
 			// ...and the student’s email…
+			// ...and the student's index position in the list
 		for (let i = 0; i < $listItems.length; i++) {
 			$( $listItems[i] ).hide();
 			let student = $( $listItems[i] );
@@ -71,40 +96,25 @@
     			   d.email.includes(searchTerm);
     	});
 
+    	// loop through matched array of students and show student list item at that same index
     	for (let i = 0; i < matched.length; i++) {
     		var index = matched[i].index;
     		$( $listItems[index] ).show();
     	}
 
-    	let searchCount = matched.length;
+    	let searchCount = matched.length; // determine the length of the matched array
+    	$list.find('.search-alert').remove(); // remove search alert
 
-    	$list.find('.search-alert').remove();
+    	// if no matches found for user's search criteria display an alert
+    	// else build the pagination based on the number of results returned
     	if (matched.length === 0) {    		
-    		$list.append('<h3 class="search-alert">No results found for current search</h3>')
+    		$list.append('<h3 class="search-alert">No results found for current search. Please alter your search criteria.</h3>')
     	} else {
     		buildPagination(calcPagesNeeded(searchCount));
     	}
-		
-    	// If there’s no “matched” students…
-           // ...display a “no student’s found” message
-   		// If over ten students were found…
-           // ...call appendPageLinks with the matched students
-	   // Call showPage to show first ten students of matched list
-
 	}
 
-	function refreshResults() {
-		var searchTerm = getSearchTerms();
-		if ( searchTerm === "" ) {
-			for (let i = 0; i < $listItems.length; i++) {
-				$( $listItems[i] ).show();
-			}
-			buildPagination(pagesNeeded);
-		}
-	}
-
-	$search.on("input", displaySearchResults)
-	$search.on("input", refreshResults)
+	// display user's results on search submit
 	$searchBtn.on("click", displaySearchResults)
 
 	/**
@@ -122,18 +132,19 @@
 	 */
 	function buildPagination(len) {
 		removePagination();
-		var html = '<div class="pagination"><ul>';
-		for (let i = 0; i < len; i++) {
-			html += '<li><a href="#">' + (i+1) + '</a></li>';
-		}
-		html += '</ul></div>';
-		$page.append(html);
-		showPage(1, $( 'li.student-item' ))
-		$( '.pagination ul').on("click", getPage)
-		setActivePage(1, $( '.pagination li' ));	
-		$pagination = $( '.pagination' ); //u update DOM reference
+		if (len > 1) {
+			var html = '<div class="pagination"><ul>';
+			for (let i = 0; i < len; i++) {
+				html += '<li><a href="#">' + (i+1) + '</a></li>';
+			}
+			html += '</ul></div>';
+			$page.append(html);
+			showPage(1, $( 'li.student-item' ))
+			$( '.pagination ul').on("click", getPage)
+			setActivePage(1, $( '.pagination li' ));
+		}	
+		$pagination = $( '.pagination' ); // update DOM reference
 	}
-
 	buildPagination(pagesNeeded);
 
 })();
